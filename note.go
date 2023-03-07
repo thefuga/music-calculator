ad919b6b30685e3819fb8bfd8c497fd346bc9fd9
Erick C. Fuga <erickcfuga@gmail.com>
2023-03-07 13:11:49 +0000

    add flat notes

--- note.go
+++ note.go
@@ -1,16 +1,21 @@
 package main
 
 var (
+	Ab = Note{11, "Ab"}
 	A  = Note{0, "A"}
 	Ax = Note{1, "A#"}
+	Bb = Note{1, "Bb"}
 	B  = Note{2, "B"}
 	C  = Note{3, "C"}
 	Cx = Note{4, "C#"}
+	Db = Note{4, "Db"}
 	D  = Note{5, "D"}
 	Dx = Note{6, "D#"}
+	Eb = Note{6, "Eb"}
 	E  = Note{7, "E"}
 	F  = Note{8, "F"}
 	Fx = Note{9, "F#"}
+	Gb = Note{9, "Gb"}
 	G  = Note{10, "G"}
 	Gx = Note{11, "G#"}
 )
